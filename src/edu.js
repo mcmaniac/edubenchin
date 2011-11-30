6caf8ff91864c4904f3d8c0a50ec12524c6533e5
Nils Schweinsberg <mail@n-sch.de>
2011-11-30 18:03:48 +0100

    new feature: read weight from GET url parameters

--- src/edu.js
+++ src/edu.js
@@ -1,3 +1,15 @@
+// Read a page's GET URL variables and return them as an associative array.
+function getUrlVars() {
+  var vars = [], hash;
+  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+  for(var i = 0; i < hashes.length; i++) {
+    hash = hashes[i].split('=');
+    vars.push(hash[0]);
+    vars[hash[0]] = hash[1];
+  }
+  return vars;
+}
+
 function kgToLbs(kg) {
   return kg * 2.20462262;
 };
@@ -7,8 +19,7 @@ function calcEdus(lbs) {
   return edus.toFixed(2);
 };
 
-function tellme() {
-
+function submitted() {
   var iambenching = $('input[name=iambenching]').val();
   if (isNaN(iambenching) || iambenching <= 0)
     return;
@@ -17,7 +28,12 @@ function tellme() {
   if (lbsorkg == "kg")
     iambenching = kgToLbs(iambenching);
 
+  tellme(iambenching);
+}
+
+function tellme(iambenching) {
   var edus = calcEdus(iambenching);
+
   var msg;
   if (edus < 1) {
     msg = "Ha, you suck! That's only " + edus + " edus.";
@@ -37,11 +53,21 @@ function tellme() {
 };
 
 $(document).ready(function() {
-  $("input[name=iambenching]").select();
-  $("input[name=iambenching], select[name=lbsorkg]").keyup(function(event) {
-    if (event.which == 13) {
-      event.preventDefault();
-      tellme();
-    }
-  });
+
+  var url_vars = getUrlVars()
+  var url_kg   = url_vars["kg"]
+  var url_lbs  = url_vars["lbs"]
+  if (!isNaN(url_lbs)) {
+    tellme(url_lbs);
+  } else if (!isNaN(url_kg)) {
+    tellme(kgToLbs(url_kg));
+  } else {
+    $("input[name=iambenching]").select();
+    $("input[name=iambenching], select[name=lbsorkg]").keyup(function(event) {
+      if (event.which == 13) {
+        event.preventDefault();
+        submitted();
+      }
+    });
+  }
 });
